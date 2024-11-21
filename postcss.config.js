7b46d177ae5d5bd78e106f2d2c122e160fe6db49
martekn <marte.mk@hotmail.com>
2024-11-21 11:58:51 +0100

    Setup sass and sass related packages

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+import postcssPresetEnv from "postcss-preset-env";
+
+export default {
+  plugins: [
+    postcssPresetEnv({
+      stage: 3,
+      features: {
+        "logical-properties-and-values": FALSE,
+        "text-decoration-shorthand": true,
+      },
+    }),
+  ],
+};
